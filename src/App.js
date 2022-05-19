26ff21d6fe90d8bf3c2f17dd9407de4a3471f58b
georgetegral <jorgerenemg@icloud.com>
2022-05-19 16:08:51 -0500

    Added dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/home';
+import Avatars from './views/avatars';
 import MainLayout from "./layouts/main";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/avatars" element={<Avatars />}/>
       </Routes>
     </MainLayout>
     </>
