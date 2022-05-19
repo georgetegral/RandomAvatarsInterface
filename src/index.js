9fe86c5d1668e3cd39237a9e92690f816b9d709b
georgetegral <jorgerenemg@icloud.com>
2022-05-19 17:17:57 -0500

    Updated index.js for React v18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import { Web3ReactProvider } from '@web3-react/core'
 import { getLibrary } from './config/web3';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
